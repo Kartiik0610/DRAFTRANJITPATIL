1e0fe6f64177229ca7eabc1c9f605af68a44f87b
Kartik Chauhan <kartikchauhan 0610@gmail.com>
2026-09-10 15:25:23 +0530

    Add Vercel Analytics tracking script to all pages

--- JS/layout.js
+++ JS/layout.js
@@ -56,6 +56,16 @@ function setFavicon() {
 }
 setFavicon();
 
+/* ---------- VERCEL ANALYTICS ---------- */
+function initVercelAnalytics() {
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+  const script = document.createElement('script');
+  script.defer = true;
+  script.src = '/_vercel/insights/script.js';
+  document.head.appendChild(script);
+}
+initVercelAnalytics();
+
 /* ---------- INIT ---------- */
 loadLayout();
 
